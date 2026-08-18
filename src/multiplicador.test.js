dc6311d0320b2f6eeba1321e79a7de6b676fb2f7
isa-hida <isabelhidalgoavilaa@gmail.com>
2026-08-18 10:24:04 -0400

    feat: multiplicador

---
+++ src/multiplicador.test.js
@@ -0,0 +1,9 @@
+import sumar from "./multiplicador.js";
+
+describe("Multiplicar", () => {
+    it("deberia multiplicar dos numeros", () => {
+        expect(multiplicar(3, 2)).toEqual(6);
+    });
+});
+
+
--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,32 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+// Suma
+const firstSum = document.querySelector("#sumar-form #primer-numero");
+const secondSum = document.querySelector("#sumar-form #segundo-numero");
+const formSum = document.querySelector("#sumar-form");
+const divSum = document.querySelector("#sumar-form").nextElementSibling;
 
-form.addEventListener("submit", (event) => {
+formSum.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = Number.parseInt(firstSum.value);
+  const secondNumber = Number.parseInt(secondSum.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  divSum.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+// Multiplicacion
+const firstMult = document.querySelector("#multiplicar-form #primer-numero");
+const secondMult = document.querySelector("#multiplicar-form #segundo-numero");
+const formMult = document.querySelector("#multiplicar-form");
+const divMult = document.querySelector("#multiplicar-form").nextElementSibling;
+
+formMult.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(firstMult.value);
+  const secondNumber = Number.parseInt(secondMult.value);
+
+  divMult.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
